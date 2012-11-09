cc43b4e42bcef2ea62baa6a65a5fb4a0374a271a
bottlezz <bottlezz2002@gmail.com>
2012-11-09 15:45:03 -0800

    path-->fs

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ http.createServer(function (request, response) {
             break;
     }
      
-    path.exists(filePath, function(exists) {
+    fs.exists(filePath, function(exists) {
      
         if (exists) {
             fs.readFile(filePath, function(error, content) {
