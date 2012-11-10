fe06c48e76c1c44b86500bd4e71e603594649b73
Shaun Lai <shuanl@sfu.ca>
2012-11-09 17:03:53 -0800

    correct server

--- server.js
+++ server.js
@@ -1,58 +1,3 @@
-/*
-var http = require('http');
-var fs = require('fs');
-var io = require('socket.io');
-var path = require('path');
- 
-http.createServer(function (request, response) {
- 
-    console.log('request starting...');
-     
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.htm';
-         
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-    }
-     
-    fs.exists(filePath, function(exists) {
-     
-        if (exists) {
-            fs.readFile(filePath, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                }
-                else {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                }
-            });
-        }
-        else {
-            response.writeHead(404);
-            response.end();
-        }
-    });
-     
-});
-io.listen(http);
-http.listen(8080);
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
-*/
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
@@ -61,7 +6,7 @@ var app = require('http').createServer(handler)
 app.listen(8080);
 
 function handler (req, res) {
-	var filePath=__dirname+req.rul;
+    var filePath=__dirname+req.rul;
     var extname = path.extname(filePath);
     console.log(req.url);
 
