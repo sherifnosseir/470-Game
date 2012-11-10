0702a3613aa8c49e6de5f046da7edfc4bf79adb7
Shaun Lai <shuanl@sfu.ca>
2012-11-09 18:28:54 -0800

    awhoelbunchofstuff

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ var app = require('http').createServer(handler)
 
 app.listen(8000);
 
-var id = 0;
-
 function handler (req, res) {
     var filePath=__dirname+req.rul;
     var extname = path.extname(filePath);
@@ -25,13 +23,82 @@ function handler (req, res) {
   });
 }
 
-io.sockets.on('connection', function (socket) {
+var id = 0;
+var tanksArray = Array();
 
-    socket.on('connect', function() {
-        id++;
-        var newTank = Object();
-        io.sockets.emit('setID', id);
+
+
+
+setInterval(function() {
+    io.sockets.volatile.emit('draw', tanksArray);
+}, 1000);
+
+
+
+io.sockets.on('connection', function(socket) {
+
+
+    id++;
+    var newTank = Object();
+    newTank.id = id;
+    newTank.x = 0;  // tank coordinates
+    newTank.y = 0;
+    tanksArray[tanksArray.length] = newTank;
+    io.sockets.emit('setID', id);
+    socket.set('idClient', id);
+
+    // movement [server side]
+    socket.on('move_left', function() {
+        socket.get('idClient', function(err, idClient) {
+            var index = 0;
+            for (i=0; i<tanksArray.length; i++) {
+                if (tanksArray[i].id == idClient) {
+                    index = i;
+                }
+            };
+            if (tanksArray[index].x <= 0) {
+                tanksArray[index].x = 500;
+            }
+            else {
+                tanksArray[index].x = tanksArray[index].x - 10;
+            }
+        });       
     });
+
+    socket.on('move_right', function() {
+        socket.get('idClient', function(err, idClient) {
+            var index = 0;
+            for (i=0; i<tanksArray.length; i++) {
+                if (tanksArray[i].id == idClient) {
+                    index = i;
+                }
+            };
+            if (tanksArray[index].x > 500) {
+                tanksArray[index].x = 0;
+            }
+            else {
+                tanksArray[index].x = tanksArray[index].x + 10;
+            }
+        });
+    });
+
+
+
+    socket.on('disconnect', function() {
+        socket.get('idClient', function(err, idClient) {
+            var index = 0;
+            for (i=0; i<tanksArray.length; i++) {
+                if (tanksArray[i].id == idClient) {
+                    index = i;
+                }
+            };
+            tanksArray.splice(index, 1);
+            console.log('Disconnect', idClient);
+            console.log(tanksArray.length);
+        });
+    });
+
+
 
 
 });
