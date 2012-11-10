71f5ebe8f2907c7e0a20fc44ba8d0dbc1bc40354
Shaun Lai <shuanl@sfu.ca>
2012-11-09 17:30:36 -0800

    asease

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 var http = require('http');
 var fs = require('fs');
@@ -87,4 +88,40 @@ io.sockets.on('connection', function (socket) {
 });
 
 
+=======
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+  , path = require('path')
+
+app.listen(8080);
+
+function handler (req, res) {
+    var filePath=__dirname+req.rul;
+    var extname = path.extname(filePath);
+    console.log(req.url);
+
+    
+  fs.readFile(__dirname + req.url,
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+    /asezse
+    
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
+>>>>>>> asease
 console.log('Server running');
