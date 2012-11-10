338df39d8c79d39483c218e688a31c61c705272d
bottlezz <bottlezz2002@gmail.com>
2012-11-09 16:58:54 -0800

    server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
 var http = require('http');
 var fs = require('fs');
@@ -87,4 +88,51 @@ io.sockets.on('connection', function (socket) {
 });
 
 
+=======
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+ 
+http.createServer(function (request, response) {
+ 
+    console.log('request starting...');
+     
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.htm';
+         
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    fs.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    response.writeHead(500);
+                    response.end();
+                }
+                else {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                }
+            });
+        }
+        else {
+            response.writeHead(404);
+            response.end();
+        }
+    });
+     
+}).listen(8000);
+ 
+>>>>>>> newl
 console.log('Server running');
