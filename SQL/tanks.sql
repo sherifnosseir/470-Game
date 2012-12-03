2f2668c01e124b6441abec4dba8702a50221f823
Sherif Nosseir <sherifnosseir@gmail.com>
2012-12-03 01:58:31 -0800

    New dp_dump includes banned table; quick fix for node script import

--- SQL/tanks.sql
+++ SQL/tanks.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.10deb1
+-- version 3.2.4
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 02, 2012 at 05:21 PM
--- Server version: 5.5.24
--- PHP Version: 5.3.10-1ubuntu3.4
+-- Generation Time: Dec 03, 2012 at 09:56 AM
+-- Server version: 5.1.44
+-- PHP Version: 5.3.1
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -18,6 +18,27 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `tanks`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bannedUsers`
+--
+
+CREATE TABLE IF NOT EXISTS `bannedUsers` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `reason` varchar(225) DEFAULT 'Please contact <a href="#">Support</a>',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `bannedUsers`
+--
+
+INSERT INTO `bannedUsers` (`id`, `username`, `email`, `reason`) VALUES
+(1, 'testing', 'test@test.com', 'Please contact <a href="#">Support</a>');
 
 -- --------------------------------------------------------
 
@@ -56,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `date_created` varchar(255) NOT NULL,
   `db_time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
 
 --
 -- Dumping data for table `users`
@@ -75,7 +96,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `nickname`, `team_id
 (11, 'kenny2', '525ec4649a43fc5850b3e785fa5bbf11', '', '', 0, 10, '', '2012-11-26 21:06:48'),
 (12, 'george''); DROP TABLE users; --', '03004ed5e8e2c9c815191f1628b0c5ec', '', '', 0, 11, '', '2012-11-26 21:12:54'),
 (13, ';;;;', '0a28f18abcc09423321d0e54ba5694d3', '', '', 0, 12, '', '2012-11-26 21:38:43'),
-(14, 'gregoryz', '8a26fc58ddf3571c205897b2cc2609a9', '', '', 0, 13, '', '2012-11-28 02:18:18');
+(14, 'gregoryz', '8a26fc58ddf3571c205897b2cc2609a9', '', '', 0, 13, '', '2012-11-28 02:18:18'),
+(15, 'testing', 'ae2b1fca515949e5d54fb22b8ed95575', 'test@test.com', '', 0, 0, '', '2012-12-03 00:32:27');
 
 -- --------------------------------------------------------
 
